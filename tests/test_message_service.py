a4c3b3ba0e20397a278e474be494adba4848f473
k2controls <kkelly@k2controls.net>
2021-04-11 17:03:33 -0400

    test update

--- tests/test_message_service.py
+++ tests/test_message_service.py
@@ -5,7 +5,7 @@ def do_tests():
     print("Test Message Service - a smartphone connect is required!")
     print("Press LED OFF button to exit.")
     ms = MessageService()
-    messageService = ""
+    message = ""
     while message != "0,0,0,0,0,0,1,0,0": #LED OFF button 
         message = ms.get_message()
         print(message)
