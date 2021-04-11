e411f70c799f1fae979601919e20209096d16506
k2controls <kkelly@k2controls.net>
2021-04-11 16:22:59 -0400

    Updated command reference

--- RoverService.py
+++ RoverService.py
@@ -2,43 +2,45 @@
 
 '''
 import time
+from threading import Thread
 from Controllers.ControlPadController import ControlPadController
 from Controllers.ColorfulController import ColorfulController
 from Controllers.TrackerController import TrackerController
 from Controllers.ObstacleController import ObstacleController
-from Bluetooth.Commands import Commands
+from Bluetooth.Commands import Command_Type, Command_ID
 
-class RoverService():
+class RoverService(Thread):
 
     def __init__(self, command_service, rover):
         self.command_service = command_service
         self.rover = rover
-        self.controller = ControlPadController
-        self.controller.start(self.rover)
-
+        self.controller = ControlPadController(self.rover)
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+        
     def run(self):
-        while not len(self.command_service.command_queue):
-            time.sleep(.1)
-        command = self.command_service.command_queue.pop(0)
-        self.do_command(command)
-
+        while True:
+            if self.command_service.command_queue:
+                command = self.command_service.command_queue.pop(0)
+                self.do_command(command)
+               
     def do_command(self, command):
-        if command.is_mode_message():
+        if command.command_type == Command_Type.MODE:
             self.switch_controller(command)
         elif type(self.controller) == ControlPadController:
             self.controller.update(command)
         else:
             self.switch_controller(command)
         
-
     def switch_controller(self, command):
         self.controller.teardown()
-        if command.command_id == Commands.COLORFUL_START:
-            self.controller = ColorfulController()
-        elif command.command_id == Commands.TRACTING_START:
-            self.controller = TrackerController()
-        elif command.command_id == Commands.OBSTACLE_START:
-            self.controller = ObstacleController()
+        if command.command_id == Command_ID.COLORFUL_START:
+            self.controller = ColorfulController(self.rover)
+        elif command.command_id == Command_ID.TRACTING_START:
+            self.controller = TrackerController(self.rover)
+        elif command.command_id == Command_ID.OBSTACLE_START:
+            self.controller = ObstacleController(self.rover)
         else:
-            self.controller = ControlPadController()
-        self.controller.start(self.rover)
+            self.controller = ControlPadController(self.rover)
+        
